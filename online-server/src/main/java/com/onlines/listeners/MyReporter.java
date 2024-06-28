e52452d5974ad51d8494fb6e5939dc32d444c42e
Dream-wu <532142910@qq.com>
2024-06-28 10:17:37 +0800

    删除无用代码

--- online-server/src/main/java/com/onlines/listeners/MyReporter.java
+++ online-server/src/main/java/com/onlines/listeners/MyReporter.java
@@ -26,21 +26,6 @@ public class MyReporter implements IReporter {
 
     private long totalTime;
 
-    private String name;
-
-    public MyReporter() {
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-        name = formatter.format(System.currentTimeMillis());
-    }
-
-    public MyReporter(String name) {
-        this.name = name;
-        if (this.name == null) {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-            this.name = formatter.format(System.currentTimeMillis());
-        }
-    }
-
     @Override
     public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
         //通过遍历 xmlSuites 和 suites 能够获取所有测试方法的信息以及测试结果，后续可用于自定义测试报告。
@@ -48,7 +33,6 @@ public class MyReporter implements IReporter {
         // 遍历List<ISuite> suites集合
         for (ISuite suite : suites) {
             Map<String, ISuiteResult> suiteResults = suite.getResults();
-
             for (ISuiteResult suiteResult : suiteResults.values()) {
                 ITestContext testContext = suiteResult.getTestContext();
                 IResultMap passedTests = testContext.getPassedTests();
@@ -91,7 +75,6 @@ public class MyReporter implements IReporter {
         try {
             int index = 0;
             for (ITestResult result : list) {
-                String tn = result.getTestContext().getCurrentXmlTest().getParameter("testCase");
                 if (index == 0) {
                     SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
                     beginTime = formatter.format(new Date(result.getStartMillis()));
@@ -99,28 +82,7 @@ public class MyReporter implements IReporter {
                 }
                 long spendTime = result.getEndMillis() - result.getStartMillis();
                 totalTime += spendTime;
-                String status = this.getStatus(result.getStatus());
-                List<String> log = Reporter.getOutput(result);
-                for (int i = 0; i < log.size(); i++) {
-                    log.set(i, this.toHtml(log.get(i)));
-                }
-                Throwable throwable = result.getThrowable();
-                if (throwable != null) {
-                    log.add(this.toHtml(throwable.toString()));
-                    StackTraceElement[] st = throwable.getStackTrace();
-                    for (StackTraceElement stackTraceElement : st) {
-                        log.add(this.toHtml("    " + stackTraceElement));
-                    }
-                }
-                if(status == "失败"){
-                    System.out.println("case名称："+result.getName());
-                    System.out.println("测试类："+result.getInstanceName());
-                    System.out.println("耗时:"+spendTime + "ms");
-                    System.out.println("用例描述："+result.getMethod().getDescription());
-                    System.out.println("失败原因"+log);
-                }
             }
-
             try {
                 res.setDuration(totalTime);
                 res.setPassedNum(testsPass);
@@ -128,7 +90,6 @@ public class MyReporter implements IReporter {
                 res.setSkippedNum(testsSkip);
                 res.setTotalNum(testsPass + testsFail + testsSkip);
                 saveTestPlan(testsPass,testsFail,testsSkip, beginTime, totalTime);
-
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
@@ -149,36 +110,6 @@ public class MyReporter implements IReporter {
         testPlanResult.setDuration(totalTime);
         testPlanResult.setCreateTime(new java.sql.Date(new Date().getTime()));
         testPlanResultMapper.insert(testPlanResult);
-    }
-    private String getStatus(int status) {
-        String statusString = null;
-        switch (status) {
-            case 1:
-                statusString = "成功";
-                break;
-            case 2:
-                statusString = "失败";
-                break;
-            case 3:
-                statusString = "跳过";
-                break;
-            default:
-                break;
-        }
-        return statusString;
-    }
-
-    private String toHtml(String str) {
-        if (str == null) {
-            return "";
-        } else {
-            str = str.replaceAll("<", "&lt;");
-            str = str.replaceAll(">", "&gt;");
-            str = str.replaceAll(" ", "&nbsp;");
-            str = str.replaceAll("\n", "<br>");
-            str = str.replaceAll("\"", "\\\\\"");
-        }
-        return str;
     }
 
 }
