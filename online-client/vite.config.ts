d00b2dd12d2283aa6a855620012488ab102a0ab0
zhangdexi <zhangdexi@daojia-inc.com>
2024-07-05 17:20:07 +0800

    fix:修改vite配置

--- online-client/vite.config.ts
+++ online-client/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     host: true,  
     proxy: {  
       '^/patrol': {  
-        target: 'http://tracker.1024996.xyz', // 代理目标地址  
+        target: 'http://127.0.0.1:9091', // 代理目标地址
         changeOrigin: true, // 开启代理，在本地会创建一个虚拟服务端，然后发送请求的数据，同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题  
         rewrite: (path) => path.replace(/^\/patrol/, '') // 可选，如果需要重写请求路径
       },
